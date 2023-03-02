e2c0738c22b40dd3604e662798156ba3f4ad37d6
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-03-02 12:29:19 +0000

    Added Initial files for learning JavaScript

---
+++ yourPlayground.js
@@ -0,0 +1,19 @@
+// console.log('Hello World!');
+
+//variables
+
+// var name = "Abhinav Pandey";
+// console.log(name);
+
+//Taking User Input.
+//prompt() is a function that helps us get a user input through browser.
+
+/*
+var foodBill = prompt("Enter your bill amount :");
+var tipPercentage = prompt("Enter the Tip % :");
+var tipAmount = foodBill * tipPercentage / 100;
+var totalBill = Number(tipAmount) + Number(foodBill);
+console.log("Your tip amount is " + tipAmount);
+console.log("Your total bill is : " + totalBill);
+*/
+
